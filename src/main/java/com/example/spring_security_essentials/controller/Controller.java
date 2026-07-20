c1af2c7298c06e60244017428f06f22f62a59647
fernandocesar4141@gmai.com <fernandocesar4141@gmail.com>
2026-07-20 19:10:56 -0300

    Criando as rotas de login e register, sem dto

--- src/main/java/com/example/spring_security_essentials/controller/Controller.java
+++ src/main/java/com/example/spring_security_essentials/controller/Controller.java
@@ -1,14 +1,33 @@
 package com.example.spring_security_essentials.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.spring_security_essentials.model.User;
+import com.example.spring_security_essentials.service.RegisterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class Controller {
+    private final RegisterService registerService;
+    private final AuthenticationManager authenticationManager;
     @GetMapping("/public")
     public String initial(){
         return "<h1>public page</h1>";
+    }
+
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user){
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getEmail(),user.getPassword()));
+        return "User autenticado.";
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody User user){
+        registerService.register(user);
+        return "User registrado.";
     }
 }
