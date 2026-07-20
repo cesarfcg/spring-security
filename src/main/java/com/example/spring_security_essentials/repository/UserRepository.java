ea0598dd0ef63788ec3fdc379a8e4cdfc76cd7e7
fernandocesar4141@gmai.com <fernandocesar4141@gmail.com>
2026-07-19 22:56:18 -0300

    User repository, criando o buscador do userDetails

--- src/main/java/com/example/spring_security_essentials/repository/UserRepository.java
+++ src/main/java/com/example/spring_security_essentials/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.example.spring_security_essentials.repository;
 
 import com.example.spring_security_essentials.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String username)throws UsernameNotFoundException;
 
 }
