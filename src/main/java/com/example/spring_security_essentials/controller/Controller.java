7cc9430f02bc3c574009e5df286896db7c358185
fernandocesar4141@gmai.com <fernandocesar4141@gmail.com>
2026-07-22 21:18:14 -0300

    Ajeitando o controller de user

--- src/main/java/com/example/spring_security_essentials/controller/Controller.java
+++ src/main/java/com/example/spring_security_essentials/controller/Controller.java
@@ -1,12 +1,19 @@
 package com.example.spring_security_essentials.controller;
 
+import com.example.spring_security_essentials.model.User;
+import com.example.spring_security_essentials.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class Controller {
+    private final UserRepository userRepository;
     @GetMapping("/public")
     public String publicRoute(){
         return "<h1>public page</h1>";
@@ -15,5 +22,9 @@ public class Controller {
     @GetMapping("private")
     public String privateRoute(){
         return "<h1>private page</h1>";
+    }
+    @GetMapping("/list")
+    public List<User> users(){
+        return userRepository.findAll();
     }
 }
