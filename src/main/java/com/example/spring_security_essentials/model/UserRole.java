8d904215b988e2115d4fe1ca16aad5383f41c074
fernandocesar4141@gmai.com <fernandocesar4141@gmail.com>
2026-07-24 20:48:30 -0300

    Criando as roles

---
+++ src/main/java/com/example/spring_security_essentials/model/UserRole.java
@@ -0,0 +1,6 @@
+package com.example.spring_security_essentials.model;
+
+public enum UserRole {
+    ADMIN,
+    USER,
+}
--- src/main/java/com/example/spring_security_essentials/service/RegisterService.java
+++ src/main/java/com/example/spring_security_essentials/service/RegisterService.java
@@ -1,6 +1,8 @@
 package com.example.spring_security_essentials.service;
 
+import com.example.spring_security_essentials.dto.UserDTO;
 import com.example.spring_security_essentials.model.User;
+import com.example.spring_security_essentials.model.UserRole;
 import com.example.spring_security_essentials.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,9 +13,11 @@ import org.springframework.stereotype.Service;
 public class RegisterService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    public void register(User user){
-        user.setEmail(user.getEmail());
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+    public void register(UserDTO userDTO){
+        User user = new User();
+        user.setEmail(userDTO.email());
+        user.setPassword(passwordEncoder.encode(userDTO.password()));
+        user.setRole(UserRole.USER);
         userRepository.save(user);
     }
 }
